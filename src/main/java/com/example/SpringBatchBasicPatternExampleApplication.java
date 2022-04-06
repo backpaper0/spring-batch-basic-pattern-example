e2d541b266c8f110f495727e5c021201802a3560
Uragami Taichi <backpaper0@gmail.com>
2022-04-06 20:35:34 +0900

    Initial commit

---
+++ src/main/java/com/example/SpringBatchBasicPatternExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchBasicPatternExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchBasicPatternExampleApplication.class, args);
+	}
+
+}
